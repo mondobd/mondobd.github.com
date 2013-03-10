fd9f8e31b362edd19d8864e24af226fa57054248
ar <ar@c96maplab.(none)>
2013-03-10 16:21:08 +0000

    updates as confirmed by client
    * fixed data structure (see: https://support.xelera.eu/rt/Ticket/Display.html?id=18227#txn-244647)
    * swapped images

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,1 @@
-require.config({
-  shim: {
-  },
-
-  paths: {
-    hm: 'vendor/hm',
-    esprima: 'vendor/esprima',
-    jquery: 'vendor/jquery.min'
-  }
-});
- 
-require(['app'], function(app) {
-  // use app here
-  console.log(app);
-});
+f398d140.main.js
